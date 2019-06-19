b73bf90014e3a46645583695c9adb033a4d3c75d
JaewooByun <jaewoobyun1@gmail.com>
2019-06-19 13:01:36 +0900

    2019.06.19

---
+++ SearchPractice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SearchPractice
+//
+//  Created by 변재우 on 20190619//.
+//  Copyright © 2019 변재우. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+		
+		//something
+	}
+
+
+}
+
